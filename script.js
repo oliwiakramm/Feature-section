abc9ce5dfbafca011c237449e765c4583fed2d24
oliwiakramm <oliwiakramm@gmail.com>
2023-02-21 21:14:59 +0100

    implementing input

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 const modeBtn = document.querySelector(".mode__switch");
-const lightIcon = document.querySelector(".light__mode");
-const darkIcon = document.querySelector(".dark__mode");
 
 modeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
-
-  if (document.body.classList.contains("dark")) {
-    darkIcon.classList.add("hidden");
-    lightIcon.classList.remove("hidden");
-  } else {
-    darkIcon.classList.remove("hidden");
-    lightIcon.classList.add("hidden");
-  }
 });
